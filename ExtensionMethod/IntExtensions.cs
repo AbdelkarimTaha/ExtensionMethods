4a48339bd39d75395951d1b5ec0c7f7337ea1404
Karim Taha <abdelkareemtaha2014@gmail.com>
2021-10-06 00:50:25 +0200

    first commit

---
+++ ExtensionMethod/IntExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class IntExtensions
+    {
+        // We are going to use this extension method on int type.
+        //Extension methods can be added to custom, .NET Framework or third party classes, structs or interfaces.
+        //Extension methods are additional custom methods which were originally not included with the class.
+        //Extension methods can be added to custom, .NET Framework or third party classes, structs or interfaces.
+        //The first parameter of the extension method must be of the type for which the extension method is applicable, preceded by the this keyword.
+        //Extension methods can be used anywhere in the application by including the namespace of the extension method.
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            //The IsGreaterThan() method is not a method of int data type(Int32 struct).
+            //It is an extension method written by the programmer for the int data type.
+            //The IsGreaterThan() extension method will be available throughout the application by including the namespace in which it has been defined.
+            bool result = i.IsGreaterThan(100);
+
+            Console.WriteLine(result); //result = false
+        }
+    }
+}
